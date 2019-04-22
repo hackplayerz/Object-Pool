91b01a6ca71482789fbd2fc185fef605bf5865cf
Alter0 <dev.alter0@gmail.com>
2019-04-22 10:03:09 +0900

    Add files via upload

---
+++ ObjectPool.cs
@@ -0,0 +1,59 @@
+﻿using System.Collections.Generic;
+using UnityEngine;
+
+public class ObjectPool<T>  where T : MonoBehaviour
+{
+    #region Variable
+
+    private readonly List<T> _pools = new List<T>(); // Pool
+    private int _count; // Count of created object
+    private readonly T _prefab; // To instantiate prefab
+
+    #endregion
+
+    public ObjectPool(T prefab)
+    {
+        _prefab = prefab;
+    }
+    
+    /// <summary>
+    /// Initialized pool
+    /// </summary>
+    /// <param name="container">Parent of spawn transform</param>
+    public void InitPool(Transform container = null)
+    {
+        T go = Object.Instantiate(_prefab).GetComponent<T>(); // Instance
+        // Set Parent transform
+        if (container != null)
+        {
+            go.transform.SetParent(container);
+        }
+        _pools.Add(go); // Push
+        go.gameObject.SetActive(false);
+    }
+    
+    /// <summary>
+    /// Create Pooled object
+    /// </summary>
+    /// <param name="position">Spawn Position</param>
+    public void Create(Vector2 position)
+    {
+        // No more pool memory exception
+        if (_count > _pools.Count - 1)
+        {
+            // If all pool object is active
+            if (_pools[0].gameObject.activeSelf)
+            {
+                InitPool();
+            }
+            else
+            {
+                _count = 0;
+            }
+        }
+        // Spawn
+        _pools[_count].gameObject.SetActive(true);
+        _pools[_count].transform.position = position;
+        _count++;
+    }
+}
